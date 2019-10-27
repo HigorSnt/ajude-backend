9b44ba47994d7d92f9d87558d1e8999718921183
Higor Santos <higor.dantas@ccc.ufcg.edu.br>
2019-10-26 22:07:28 -0300

    inicializando o modelo do springboot
    adicionando modelo do springboot com algumas dependências

---
+++ ajude/src/test/java/org/ajude/ajude/AjudeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ajude.ajude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
