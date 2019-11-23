fe62336cb47e55a625e0a8177c5d62d939b5eeb6
mateustranquilino <mateus.tranquilino@ccc.ufcg.edu.br>
2019-11-23 13:20:39 -0300

    Will ye go, lassie, go?

--- src/main/java/org/ajude/config/WebConfig.java
+++ src/main/java/org/ajude/config/WebConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+		.allowedOrigins("*");
 	}
 }
